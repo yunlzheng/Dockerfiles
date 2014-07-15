a19a6f3000be0a61d2a3347300881b6a8db7ab4b
yunlzheng <yunl.zheng@gmail.com>
2014-07-15 16:27:40 +0800

    use fabric build images

--- fabfile.py
+++ fabfile.py
@@ -1,17 +1,20 @@
 # coding: utf-8
 from fabric.api import *
 
+
 def build():
     """
     build docker containers
     """
     with lcd('base'):
         local('pwd')
-        local('docker build -t base/java7 .')
+        local('docker build -t cloud/base .')
 
-    with lcd('jenkins'):
+
+def build_one(name='base'):
+    """
+    build image by name
+    """
+    with lcd(name):
         local('pwd')
-        local('docker build -t base/jenkins .')
-
-def start_jenkins():
-    pass
+        local('docker build -t cloud/' + name + ' .')
