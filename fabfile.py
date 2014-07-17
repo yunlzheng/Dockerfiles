d193a8285d9901d5ddd7fc81fa332fbd29454530
yunlzheng <yunl.zheng@gmail.com>
2014-07-17 14:16:29 +0800

    add fabric task build all docker in local

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import os
 from fabric.api import *
 
 
@@ -15,6 +16,18 @@ def build_one(name='base'):
     """
     build image by name
     """
-    with lcd(name):
-        local('pwd')
-        local('docker build -t cloud/' + name + ' .')
+    with settings(warn_only=True):
+        with lcd(name):
+            local('pwd')
+            local('docker build -t cloud/' + name + ' .')
+
+def build_all():
+    """
+    build all images define by Dockerfile
+    """
+    build_one('base')
+    execute_dirs = ['base', '.git', '.gitignore', 'bin', 'fabfile.py', 'fabfile.pyc', 'LICENSE', 'README.md', 'setup.sh']
+    dirs = os.listdir('.')
+    dirs = filter(lambda v: v not in execute_dirs, dirs)
+    for subdir in dirs:
+        build_one(subdir)
